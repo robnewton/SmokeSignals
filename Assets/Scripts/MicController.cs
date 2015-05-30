cf529bff97959e8e9a40e402680a2689408c4486
Rob Newton <robbienewton@gmail.com>
2015-05-30 02:04:08 -0700

    Added multiple mic activation behaviors
    Push the T button to control it. Currently it's configured to behave as
    a Hold to Talk feature where T is the keyboard key to hold down.
    
    Push to talk
    Hold to talk
    Always on
    
    Some funky behavior when transmitting the audio over the network. Need
    to track that down, so it wont work perfect right now and may crash in
    fact.

--- Assets/Scripts/MicController.cs
+++ Assets/Scripts/MicController.cs
@@ -1,33 +1,130 @@
 ﻿/**************
  * Based upon code from
  * http://forum.unity3d.com/threads/microphone-network-test.123776/
+ * 
+ * As well as some bits from the Mic Control asset in the store
+ * https://www.assetstore.unity3d.com/en/#!/content/12518
  **************/
 
 using UnityEngine;
 using System.Collections;
 
 public class MicController : Photon.MonoBehaviour {
+
+	public enum MicActivation {
+		HoldToSpeak,
+		PushToSpeak,
+		ConstantSpeak
+	}
 	
 	public int Frequency = 44100;
-
+	public float sensitivity = 100;
+	[Range(0,100)]
+	public float sourceVolume = 100;//Between 0 and 100
+	public string selectedDevice { get; private set; }
+	public float loudness { get; private set; }
+	public MicActivation activationStyle;
+	
+	private bool focused = true;
+	private int amountSamples = 256; //increase to get better average, but will decrease performance. Best to leave it
+	private int minFreq, maxFreq;
 	private int lastSample;
 	private AudioClip c;
 	private double timer;
 	
 	void Start () {
 
-		Debug.Log("Microphone is started.");
+		Debug.Log("Starting the microphone...");
 
-		if (photonView.isMine) {
-			c = Microphone.Start(null, true, 100, Frequency);
-			while(Microphone.GetPosition(null) < 0) {}
+		if (photonView.isMine)
+		{
+			if (Microphone.devices.Length == 0)
+			{
+				Debug.Log("No microphone found!");
+			}
+			else
+			{
+				if (Microphone.devices.Length < 2)
+				{
+					//If there is only one device, select it by default
+					selectedDevice = Microphone.devices[0].ToString();
+				}
+				else
+				{
+					//TODO: Allow the user to choose a mic using a VR menu
+					//For now, just use the first device again until a menu is in place
+					selectedDevice = Microphone.devices[0].ToString();
+				}
+				GetMicCaps();
+
+				//c = Microphone.Start(selectedDevice, true, 10, minFreq);
+				//while(Microphone.GetPosition(null) < 0) {}
+				StartMicrophone();
+			}
 		}
 	}
 
+	public void GetMicCaps()
+	{
+		Microphone.GetDeviceCaps(selectedDevice, out minFreq, out maxFreq);//Gets the frequency of the device
+		if ((minFreq + maxFreq) == 0)//These 2 lines of code are mainly for windows computers
+			maxFreq = 44100;
+	}
+
+	public void StartMicrophone()
+	{
+		c = Microphone.Start(selectedDevice, true, 100, minFreq);//Starts recording
+		while (!(Microphone.GetPosition(selectedDevice) > 0)){} // Wait until the recording has started
+		//GetComponent<AudioSource>().Play(); // Play the audio source!
+	}
+
+	public void StopMicrophone()
+	{
+		GetComponent<AudioSource>().Stop();//Stops the audio
+		Microphone.End(selectedDevice);//Stops the recording of the device
+	}   
+
 	void Update()
 	{
+		if (!focused)
+			StopMicrophone();
+		
+		if (!Application.isPlaying)
+			StopMicrophone();
+
+		//Calculate loudness
+		GetComponent<AudioSource>().volume = (sourceVolume / 100);
+		loudness = GetAveragedVolume() * sensitivity * (sourceVolume / 10);
+
+		//Hold To Speak!!
+		if (activationStyle == MicActivation.HoldToSpeak)
+		{
+			if (Microphone.IsRecording(selectedDevice) && Input.GetKey(KeyCode.T) == false)
+				StopMicrophone();
+		
+			if (Input.GetKeyDown(KeyCode.T)) //Push to talk
+				StartMicrophone();
+
+			if (Input.GetKeyUp(KeyCode.T))
+				StopMicrophone();
+		}
+		
+		//Push To Talk!!
+		if (activationStyle == MicActivation.PushToSpeak)
+		{
+			if (Input.GetKeyDown(KeyCode.T))
+			{
+				if (Microphone.IsRecording(selectedDevice))
+					StopMicrophone();
+				
+				else if (!Microphone.IsRecording(selectedDevice))
+					StartMicrophone();
+			}
+		}
+
+		//Slightly delayed networking transmission for better quality
 		timer += Time.deltaTime;
-		if (photonView.isMine && timer > .1f)
+		if (photonView.isMine && timer > .1f && Microphone.IsRecording(selectedDevice))
 		{
 			timer = 0;
 			int pos = Microphone.GetPosition(null);
@@ -43,15 +140,28 @@ public class MicController : Photon.MonoBehaviour {
 		}
 	}
 	
+	float GetAveragedVolume()
+	{
+		float[] data = new float[amountSamples];
+		float a = 0;
+		GetComponent<AudioSource>().GetOutputData(data,0);
+		foreach(float s in data) {
+			a += Mathf.Abs(s);
+		}
+		return a/amountSamples;
+	}
+	
 	[RPC]
-	public void Send(byte[] ba, int chan) {
+	public void Send(byte[] ba, int chan)
+	{
 		float[] f = ToFloatArray(ba);
-		GetComponent<AudioSource>().clip = AudioClip.Create("test", f.Length, chan, Frequency,true,false);
+		GetComponent<AudioSource>().clip = AudioClip.Create("test", f.Length, chan, Frequency, true, false);
 		GetComponent<AudioSource>().clip.SetData(f, 0);
 		if (!GetComponent<AudioSource>().isPlaying) GetComponent<AudioSource>().Play();
 	}
 	
-	public byte[] ToByteArray(float[] floatArray) {
+	public byte[] ToByteArray(float[] floatArray)
+	{
 		int len = floatArray.Length * 4;
 		byte[] byteArray = new byte[len];
 		int pos = 0;
@@ -63,12 +173,23 @@ public class MicController : Photon.MonoBehaviour {
 		return byteArray;
 	}
 	
-	public float[] ToFloatArray(byte[] byteArray) {
+	public float[] ToFloatArray(byte[] byteArray)
+	{
 		int len = byteArray.Length / 4;
 		float[] floatArray = new float[len];
 		for (int i = 0; i < byteArray.Length; i+=4) {
 			floatArray[i/4] = System.BitConverter.ToSingle(byteArray, i);
 		}
 		return floatArray;
+	}
+	
+	void OnApplicationFocus(bool focus)
+	{
+		focused = focus;
+	}
+	
+	void OnApplicationPause(bool focus)
+	{
+		focused = focus;
 	}
 }
